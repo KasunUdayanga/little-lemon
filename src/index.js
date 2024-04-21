3e0cfee60ff8ff64b324e168ada167218e57fdc3
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-04-21 18:21:58 +0530

    add more features

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import Display from './component/Display';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Header from './component/Header';
+import reportWebVitals from './reportWebVitals';
+import Footer from './component/Footer';
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    <Display/>
+    <Footer/>
   </React.StrictMode>
 );
 
