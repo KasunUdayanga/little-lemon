825c9775cf18dff05fc27d4bdb2f092ca6dddd62
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-04-21 08:48:33 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
- 
+ <div>
+  <header className="App-header">
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      react amarui
+    </a>
+  </header>
+ </div>
   );
 }
 
