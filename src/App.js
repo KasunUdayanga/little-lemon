292ba3aa9fe2e429cfb3d587686812f1278e701c
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-04-21 16:58:41 +0530

    add meta tag

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 
 function App() {
   return (
+    
  <div>
+  <meta name="description" content="your text goes here"/>
+  <meta name="og:title" content=""/>
+  <meta name="og:description" content=""/>
+  <meta name="og:image" content=""/>
   <header className="App-header">
     <p>
-      Edit <code>src/App.js</code> and save to reload.
+      Edit src/App.js and save to reload.
     </p>
     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
       react amarui
